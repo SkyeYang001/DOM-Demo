44bef740397b4a2a7b89b5e9ff7de0739eb6f077
SkyeYang001 <143848467+SkyeYang001@users.noreply.github.com>
2024-02-07 14:12:24 -0800

    Lecture
    2/7/24

--- app.js
+++ app.js
@@ -9,23 +9,49 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // optionally update when fields update
     // tbd
+    document.getElementById("name").addEventListener 'change', function (formData) {
+      let formData = document.getElementById('nameOutput');
+    }
+
+    nameOutput.innerText = eventData.Target.value;
+
+    document.getElementById("type").addEventListener 'change', function (formData) {
+      let formData = document.getElementById('typeOutput');
+    }
+
+    nameOutput.innertext = eventData.Target.value;
 
     // process form Data
-    document.getElementById("myForm").addEventListener("submit", function (e) {
-        e.preventDefault();
+    document.getElementById("myForm").addEventListener("submit", function (eventData) {
+      console.log(eventdata.target)
+        e.preventDefault(); // stops
       
         var formData = new FormData(form);
+        formData = Object.function(FormData);
         // output as an object
-        console.log(Object.fromEntries(formData));
+
       
         // ...or iterate through the name-value pairs
-        for (var pair of formData.entries()) {
+      /* for (var pair of formData.entries()) {
           console.log(pair[0] + ": " + pair[1]);
         }
-      });
+      }); */ 
+
+      //
+      document.getElementById("cartname");
+      let info = document.getElementById("info");
+      cartname.innerText = formData.name;
+      info.innerText = formData.type;
     // Log readiness to console
     console.log("Ready");
 });
+
+/* let info = document.getElementById("info");
+info.classList.add ('test');
+let nameOutput = document.getElementById */
+
+let certificate = document.getElementById("info")
+info.innerText = eventData
 
 function processForm(form){
 
